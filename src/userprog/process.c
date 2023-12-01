a7bb8782d0f7dbd9299d946bef9a9e3b0745dce6
ljy2855 <ljy2855@naver.com>
2023-12-01 13:13:50 +0900

    feat: Implement load segment to vm_entry

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -573,29 +573,40 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       size_t page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE;
       size_t page_zero_bytes = PGSIZE - page_read_bytes;
 
-      /* Get a page of memory. */
-      uint8_t *kpage = palloc_get_page (PAL_USER);
-      if (kpage == NULL)
-        return false;
+      // /* Get a page of memory. */
+      // uint8_t *kpage = palloc_get_page (PAL_USER);
+      // if (kpage == NULL)
+      //   return false;
 
-      /* Load this page. */
-      if (file_read (file, kpage, page_read_bytes) != (int) page_read_bytes)
-        {
-          palloc_free_page (kpage);
-          return false; 
-        }
-      memset (kpage + page_read_bytes, 0, page_zero_bytes);
+      // /* Load this page. */
+      // if (file_read (file, kpage, page_read_bytes) != (int) page_read_bytes)
+      //   {
+      //     palloc_free_page (kpage);
+      //     return false; 
+      //   }
+      // memset (kpage + page_read_bytes, 0, page_zero_bytes);
 
-      /* Add the page to the process's address space. */
-      if (!install_page (upage, kpage, writable)) 
-        {
-          palloc_free_page (kpage);
-          return false; 
-        }
+      // /* Add the page to the process's address space. */
+      // if (!install_page (upage, kpage, writable)) 
+      //   {
+      //     palloc_free_page (kpage);
+      //     return false; 
+      //   }
+      struct vm_entry *new = (struct vm_entry *)malloc(sizeof(struct vm_entry));
+      new->type = VM_BIN;
+      new->vaddr = upage;
+      new->writable = writable;
+      new->read_bytes = read_bytes;
+      new->zero_bytes = zero_bytes;
+      new->file = file;
+      new->offset = ofs;
+      new->is_loaded = false;
+      insert_vm_entry(&thread_current()->vm_table, new);
 
       /* Advance. */
       read_bytes -= page_read_bytes;
       zero_bytes -= page_zero_bytes;
+      ofs += page_read_bytes;
       upage += PGSIZE;
     }
   return true;
@@ -617,6 +628,13 @@ setup_stack (void **esp)
         *esp = PHYS_BASE;
       else
         palloc_free_page (kpage);
+      struct vm_entry *new = (struct vm_entry *)malloc(sizeof(struct vm_entry));
+      memset(new, 0, sizeof(struct vm_entry));
+      new->type = VM_BIN;
+      new->vaddr = kpage;
+      new->is_loaded = true;
+      new->writable = true;
+      insert_vm_entry(&thread_current()->vm_table, new);
     }
   return success;
 }
