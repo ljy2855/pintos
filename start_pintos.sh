c6d3c25b43b35f53334933393f4b6b70856237bb
ljy2855 <ljy2855@naver.com>
2024-11-16 11:53:43 +0000

    feat: init filesys

--- start_pintos.sh
+++ start_pintos.sh
@@ -3,27 +3,25 @@
 # 사용자의 이름이나 고유한 값을 사용하여 컨테이너 이름 설정
 USER_NAME=$(whoami)
 CONTAINER_NAME="pintos_${USER_NAME}"
+DOCKER_COMPOSE_FILE="docker-compose.yaml"
+
 
 # docker-compose.yaml 파일 생성
-cat <<EOL > docker-compose.yaml
+cat <<EOL > ${DOCKER_COMPOSE_FILE}
 version: '3'
 services:
   dev:
     image: thierrysans/pintos
-    container_name: ${CONTAINER_NAME}
+    container_name: pintos_${USER_NAME}
     volumes:
       - .:/pintos
     working_dir: /pintos
-    hostname: ${CONTAINER_NAME}
+    hostname: pintos_${USER_NAME}
     tty: true
-    entrypoint: bash
+    entrypoint: bash -c "echo 'export TMOUT=0' >> ~/.bashrc && bash"
 EOL
 
-echo "docker-compose.yaml 파일이 생성되었습니다."
-echo "컨테이너 이름은 ${CONTAINER_NAME} 입니다."
-
-# Docker Compose로 컨테이너를 실행
-docker-compose up -d --build
-
+# 사용자별 docker-compose 파일로 컨테이너 실행
+docker-compose -p pintos_${USER_NAME} up -d --build
 # 실행된 컨테이너로 bash 접속
 docker exec -it ${CONTAINER_NAME} bash
